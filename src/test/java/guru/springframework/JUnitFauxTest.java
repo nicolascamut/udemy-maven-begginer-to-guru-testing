8be90424e1f9f8855a28b97368c321b1fda9650a
Nicolas Camut <nicolas.camut@paf.com>
2024-06-01 18:26:32 +0300

    Junit 4 and 5

---
+++ src/test/java/guru/springframework/JUnitFauxTest.java
@@ -0,0 +1,11 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+public class JUnitFauxTest {
+
+    @Test
+    void someTestForJUnit5() {
+        System.out.println("I ran for JUnit 5");
+    }
+}
