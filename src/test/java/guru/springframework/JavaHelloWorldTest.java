ef4cde4fc0b764130b1654ce1bf2093f1dc83cdf
Nicolas Camut <nicolas.camut@paf.com>
2024-06-01 18:03:56 +0300

    Junit 4

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,13 @@
+package guru.springframework;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class JavaHelloWorldTest {
+
+    @Test
+    public void getHello() {
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+        assertEquals("Hello World", javaHelloWorld.getHello());
+    }
+}
